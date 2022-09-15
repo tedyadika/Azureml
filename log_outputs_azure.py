d6ea6559adf2549d80992341a87218a8d01c01b6
tedyadika <66624675+tedyadika@users.noreply.github.com>
2022-09-15 11:24:55 +0200

    Add files via upload

---
+++ log_outputs_azure.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 18:59:16 2022
+
+@author: aokelo
+"""
+
+from azureml.core import Workspace,Datastore,Dataset,Experiment
+
+
+## create or get workspace 
+ws = Workspace.get(name='dscglobalceedamlt',
+                    subscription_id='8b20e38c-72fb-4030-a702-128e713479ca',
+                    resource_group='DSCGlobalCEEDTANLTst',
+                    #create_resource_group=False,
+                    #location='WestEurope'
+                    )
+
+our_store =Datastore.get(ws, 'azure_sdk_blob')
+
+
+
+### Create experiment 
+experiment = Experiment(workspace = ws,
+                        name = 'picks_experiment')
+
+### run an experiment
+new_run = experiment.start_logging()
+
+#### do your staff here
+##########################################################################################
+
+### get the datasets 
+ml_dataset = Dataset.get_by_name(ws, 'picks_dataset_sdk')
+df = ml_dataset.to_pandas_dataframe()
+
+nulldf = df.isnull().sum()
+
+for col in df.columns:
+    new_run.log(col, nulldf[col])
+
+
+
+###### complete the experiment
+
+new_run.complete()
